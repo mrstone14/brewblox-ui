c90128f003c5acfafe30694920efbb13a88fc724
mrstone14 <73611198+mrstone14@users.noreply.github.com>
2025-01-03 14:54:44 -0800

    Update WebframeDisplayPartComponent.vue

--- src/plugins/builder/parts/WebframeDisplayPartComponent.vue
+++ src/plugins/builder/parts/WebframeDisplayPartComponent.vue
@@ -33,29 +33,51 @@ const counterScale = computed<number>(
 <template>
   <svg v-bind="{ width, height }">
     <foreignObject v-bind="{ width, height }">
-      <div v-if="editable || !url" class="fit column text-grey justify-center items-center q-gutter-y-md">
+      <div
+        v-if="editable || !url"
+        class="fit column text-grey justify-center items-center q-gutter-y-md"
+      >
         <q-icon name="mdi-image-frame" size="xl" />
         <div class="col-auto text-center">
           {{ url || "No iframe URL set" }}
         </div>
       </div>
-      <iframe v-else :src="url" referrerpolicy="no-referrer" allowfullscreen :style="{
-        border: 'none',
-        transform: `scale(${scale})`,
-        transformOrigin: '0 0',
-        width: 100 * counterScale + '%',
-        height: 100 * counterScale + '%',
-        'pointer-events': 'auto',
-      }" />
+      <iframe
+        v-else
+        :src="url"
+        referrerpolicy="no-referrer"
+        allowfullscreen
+        :style="{
+          border: 'none',
+          transform: `scale(${scale})`,
+          transformOrigin: '0 0',
+          width: 100 * counterScale + '%',
+          height: 100 * counterScale + '%',
+          'pointer-events': 'auto',
+        }"
+      />
     </foreignObject>
     <BuilderInteraction v-if="editable" v-bind="{ width, height }">
       <q-menu touch-position context-menu>
         <q-list>
-          <SizeMenuContent :min="MIN_SIZE" :max="MAX_SIZE" :default="DEFAULT_SIZE" />
-          <TextMenuContent :settings-key="URL_KEY" label="Edit URL"
-            message="URL must include the http:// or https:// prefix." />
-          <SliderMenuContent :min="10" :max="300" :default="100" :settings-key="WEBFRAME_SCALE_KEY" label="Adjust zoom"
-            postfix="%" />
+          <SizeMenuContent
+            :min="MIN_SIZE"
+            :max="MAX_SIZE"
+            :default="DEFAULT_SIZE"
+          />
+          <TextMenuContent
+            :settings-key="URL_KEY"
+            label="Edit URL"
+            message="URL must include the http:// or https:// prefix."
+          />
+          <SliderMenuContent
+            :min="10"
+            :max="300"
+            :default="100"
+            :settings-key="WEBFRAME_SCALE_KEY"
+            label="Adjust zoom"
+            postfix="%"
+          />
         </q-list>
       </q-menu>
     </BuilderInteraction>
